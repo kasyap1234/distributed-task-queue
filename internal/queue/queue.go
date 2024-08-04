9197cd83753d800fb677ee1f583c8c1b392a497d
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-08-04 11:08:04 +0530

    queue updated

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -48,30 +48,33 @@ func (q *RedisQueue) Dequeue()(*models.Job,error){
 	if len(result) == 0 {
 		return nil,ErrQueueEmpty
 	}
-}
-func (q *RedisQueue) EnqueueDeadLetter(job *models.Job) error {
-	jobData, err := json.Marshal(job)
-	if err != nil {
-		log.Fatal(err)
-		return err
+	var job models.Job 
+	if err :=json.Unmarshal([]byte(result[0]),&job); err !=nil {
+		return nil,err
 	}
-	return q.client.LPush(context.Background(), "dead_letter_queue", jobData).Err()
+	return &job,nil
+}
+func (q *RedisQueue) EnqueueDeadLetter(job *models.Job)error {
+	jobData, err :=json.Marshal(job); 
+	if err !=nil {
+		return err; 
+	}
+return q.client.LPush(context.Background(),"dead_letter_queue",jobData).Err()
 
 }
-func (q *RedisQueue) GetJobs() ([]models.Job, error) {
-	result, err := q.client.LRange(context.Background(), "job_queue", 0, -1).Result()
-	if err != nil {
-		return nil, err
+func (q *RedisQueue) GetJobs() ([]models.Job, error){
+	jobsData,err := q.client.ZRange(context.Background(),"job_queue",0,-1).Result(); 
+	if err !=nil {
+		return nil , err 
 	}
-	jobs := make([]models.Job, len(result))
-	for i, result := range result {
-		var job models.Job
-		if err := json.Unmarshal([]byte(result), &job); err != nil {
-			return nil, err
+	jobs :=make([]models.Job,len(jobsData)); 
+	for i,jobData :=range jobsData {
+		var job models.Job 
+		if err :=json.Unmarshal([]byte(jobData),&job); err !=nil {
+			return nil,err
 		}
 		jobs[i] = job
 
 	}
-	return jobs, nil
-
+	return jobs,nil
 }
