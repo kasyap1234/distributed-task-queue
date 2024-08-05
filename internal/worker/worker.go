65642553c1a97d3a7ae5a9422baa0ee0d14a7cdd
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-08-05 09:10:01 +0530

    worker updated

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,23 +1,64 @@
-package worker 
+package worker
+
 import (
 	"context"
+	"distributed-task-queue/internal/logger"
+	"distributed-task-queue/internal/models"
+	"distributed-task-queue/internal/queue"
 	"fmt"
 	"log"
 	"sync"
 	"time"
-	"distributed-task-queue/internal/models"
-	"distributed-task-queue/internal/queue"
-	"distributed-task-queue/internal/logger"
 
+	"go.uber.org/zap"
 )
 type Worker struct {
 	queue *queue.RedisQueue
-	maxRetries int 
+	maxRetries int
+	rateLimit int 
+	numWorkers int 
+
 }
-func NewWorker(q *queue.RedisQueue, maxRetries int) *Worker {
-	return &Worker {
-		queue : q , 
-		maxRetries: maxRetries
+
+func NewWorker(q *queue.RedisQueue,maxRetries,rateLimit int) *Worker {
+	return &Worker{
+		queue: q,
+		maxRetries: maxRetries,
+		rateLimit: rateLimit,
 	}
 }
-func (w *Worker) Start
+func (w *Worker) Start (ctx context.Context,wg *sync.WaitGroup){
+	defer wg.Done()
+	for  i :=0; i< w.numWorkers ; i++ {
+		wg.Add(1); 
+		go w.runWorker(ctx,wg)
+	}
+}
+func (w *Worker) runWorker (ctx context.Context,wg *sync.WaitGroup){
+	defer wg.Done()
+	rateLimiter :=time.Tick(time.Second /time.Duration(w.rateLimit))
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Logger.Info("Worker shutting Down"); 
+			return 
+		case <-rateLimiter:
+			job,err :=w.queue.Dequeue(); 
+
+			// if queue is empty then wait for a second 
+			if err ==queue.ErrQueueEmpty{
+				time.Sleep(time.Second); 
+				continue
+			}
+			if err!=nil {
+				logger.Logger.Error("Error dequeueing job",zap.Error())
+				time.Sleep(time.Second); 
+				continue 
+
+			}
+			if err :=w.processJob(job); err !=nil {
+				
+			}
+		}
+	}
+}
